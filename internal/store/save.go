28c889dc24103ff1a777509a130d222b03d8b0c2
Ollie Phillips <ollie.hpe@gmail.com>
2025-03-18 16:06:55 +0000

    index.html css formatting

--- internal/store/save.go
+++ internal/store/save.go
@@ -35,32 +35,43 @@ func WriteIndex(sources []internal.Source, folder string, refresh int) error {
 			body {
 				margin:500px;
 				margin-top:200px;
+				background-color: #bed3fb;
 			}
 			* {
-				font-family: sans-serif;
-			}	
+				font-family: courier;
+			}
+			.source {
+				padding-top:5px;
+				padding-bottom:5px;
+			}
 		</style>
 	</head>
 	<body>
+		<div class="logo">
+		</div>
 		<h2>Cached Endpoints</h2>
 		<p>The following data sources are cached locally and available:</p>
 			%s
-		<p>Last refresh: %s</p>
-		<p>Refresh Interval: %d mins</p>
+		<p>Last refresh: %s<br/>
+		Refresh Interval: %d mins</p>
 	</body>
 </html>
 `
 	htmlSources := ""
-	htmlSource := `<div>
-	<h4>%s</h4>
-	<p>Remote URL: %s</p>
-	<p>Local Endpoint: <a href="%s">/%s</a></p>
-	<hr>
+	htmlSource := `<div class="source">
+	<h3>%s</h3>
+	<p>Remote URL: %s<br/>
+	Local Endpoint: <a href="%s">/%s</a></p>
 </div>
+<hr>
 `
 
 	for _, source := range sources {
-		htmlSourceEdit := fmt.Sprintf(htmlSource, source.Name, source.Url, source.ServerFilename, source.ServerFilename)
+		url := source.Url
+		if len(source.Url) > 150 {
+			url = source.Url[:150] + "..."
+		}
+		htmlSourceEdit := fmt.Sprintf(htmlSource, source.Name, url, source.ServerFilename, source.ServerFilename)
 		htmlSources += htmlSourceEdit
 	}
 
